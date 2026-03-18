6af08eaafdad3aafa6eb8cbc0f6425ab911dc75d
Shoyebreza <68115608+shoyebreza@users.noreply.github.com>
2026-03-18 23:34:45 +0600

    user seed

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::insert([
+            ['name' => 'User 1', 'email' => 'user1@example.com', 'password' => bcrypt('password')],
+            ['name' => 'User 2', 'email' => 'user2@example.com', 'password' => bcrypt('password')],
+            ['name' => 'User 3', 'email' => 'user3@example.com', 'password' => bcrypt('password')],
+        ]);
+    }
+}
